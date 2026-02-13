5b22ae4378457c240bb838d817549764e64d33e7
Mukesh Karn <63993040+thekrishnarajput@users.noreply.github.com>
2026-02-13 17:44:42 +0530

    logo size increased

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -118,7 +118,7 @@ const Navbar = () => {
         }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16 md:h-20">
+        <div className="flex items-center justify-between h-20 md:h-24 lg:h-28">
           {/* Logo with animation */}
           <Link to="/" className="flex items-center flex-shrink-0 group">
             <motion.div
